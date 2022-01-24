e1f37412c88496a21caadb149602c14f45bcc957
peykani <peykani@gmail.com>
2022-01-24 12:07:58 +0300

    css ve js dosyaları düzenlendi

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
 function changeName() {
     var ad=document.getElementById("fullname");
-window.alert(ad.value);
+let fullname = ad.value.toUpperCase()
+
+document.getElementById('p-fullname').innerHTML=fullname;
 }
-function changeAvatar() {
-    var ad=document.getElementById("avatar");
-window.alert(ad.value);
+function changeAvatar(i) {
+    var ad=document.getElementsByClassName("avatar-s")[i];
+
+var image = document.getElementById('p-avatar');
+image.src=ad.value;
 }
 function changeBirth() {
     var ad=document.getElementById("birth");
-window.alert(ad.value);
+    const splitDate = ad.value.split("-");
+    let yil = splitDate[0], ay=splitDate[1], gun=splitDate[2];
+    let age = calculateAge(ay,gun,yil)
+    document.getElementById('p-age').innerHTML=age;
 }
 function changeInfo() {
     var ad=document.getElementById("info");
-window.alert(ad.value);
+
+document.getElementById('p-info').innerHTML=ad.value;
+}
+
+function calculateAge(birthMonth, birthDay, birthYear)
+{
+  todayDate = new Date();
+  todayYear = todayDate.getFullYear();
+  todayMonth = todayDate.getMonth();
+  todayDay = todayDate.getDate();
+  age = todayYear - birthYear;
+ 
+  if (todayMonth < birthMonth - 1)
+  {
+    age--;
+  }
+ 
+  if (birthMonth - 1 == todayMonth && todayDay < birthDay)
+  {
+    age--;
+  }
+  return age;
 }
