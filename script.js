732cc8df1d1213a5d3244d4cb84f54456456c4d7
peykani <peykani@gmail.com>
2022-01-24 08:59:38 +0300

    profil sayfası şablonu

---
+++ script.js
@@ -0,0 +1,16 @@
+function changeName() {
+    var ad=document.getElementById("fullname");
+window.alert(ad.value);
+}
+function changeAvatar() {
+    var ad=document.getElementById("avatar");
+window.alert(ad.value);
+}
+function changeBirth() {
+    var ad=document.getElementById("birth");
+window.alert(ad.value);
+}
+function changeInfo() {
+    var ad=document.getElementById("info");
+window.alert(ad.value);
+}
